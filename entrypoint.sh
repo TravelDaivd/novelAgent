211b11ec8bd79f88baee7f51147fe4d6180bd173
gulei <gulei_2713@163.com>
2026-08-11 14:11:27 +0800

    等neo4j服务器启动成功

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,6 +37,7 @@ done
 if [ "$all_model_exist" = true ]; then
     echo "【离线私有化模式】检测到本地完整三套模型，跳过魔塔下载，不访问外网"
     export MODEL_LOAD_MODE="local"
+    sleep 10
 else
     echo "【在线演示模式】缺失本地模型，开始登录魔塔批量下载3套私有模型"
     export MODEL_LOAD_MODE="online"
