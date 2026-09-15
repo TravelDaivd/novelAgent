5e764a1e1536f5ec6612297c571bfdae052fe67e
gulei <gulei_2713@163.com>
2026-09-15 16:50:45 +0800

    添加全局日志路径

--- src/tools/retrieval/graph_search.py
+++ src/tools/retrieval/graph_search.py
@@ -2,7 +2,6 @@
 import logging
 
 from tools.utils.tool_utils import ToolUtils
-from tools.utils.log_and_catch import log_and_catch
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
