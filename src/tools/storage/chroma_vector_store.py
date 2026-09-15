f1ec8d923c4dd13a15a588be28df8e1827ba341a
gulei <gulei_2713@163.com>
2026-09-15 16:43:59 +0800

    更改了全局日志文件所在路径

--- src/tools/storage/chroma_vector_store.py
+++ src/tools/storage/chroma_vector_store.py
@@ -9,7 +9,7 @@ from chromadb import EmbeddingFunction
 
 from tools.utils.tool_utils import ToolUtils
 from utils.config import *
-from tools.utils.log_and_catch import log_and_catch
+from utils.log_and_catch import log_and_catch
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
